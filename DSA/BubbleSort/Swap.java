0dd17edd8ca82a09ea8b74071f7147283367e98b
Haris Iftikhar <59412623+haris-bit@users.noreply.github.com>
2022-10-10 13:14:34 +0500

    Add files via upload

---
+++ DSA/BubbleSort/Swap.java
@@ -0,0 +1,11 @@
+public class Swap {
+    public static void swap(int[] arr, int indexOne, int indexTwo) {
+      int temp = arr[indexOne];
+      arr[indexOne] = arr[indexTwo];
+      arr[indexTwo] = temp;
+    }
+  
+    public static void main(String[] args) {
+  
+    }
+  }
