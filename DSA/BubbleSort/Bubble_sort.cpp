e96833b55c8747f80c924c4d604cd8fcd1295ff3
Soham Chowdhury <chowdhurysoham007@gmail.com>
2022-10-15 20:09:28 +0530

    Update Bubble_sort.cpp

--- DSA/BubbleSort/Bubble_sort.cpp
+++ DSA/BubbleSort/Bubble_sort.cpp
@@ -1,48 +1,41 @@
-#include <bits/stdc++.h>
-#define swap(x, y) \
-    {              \
-        x = x + y; \
-        y = x - y; \
-        x = x - y; \
-    }
+// C program for implementation of Bubble sort
+#include <stdio.h>
 
-using namespace std;
+void swap(int* xp, int* yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
 
+// A function to implement bubble sort
 void bubbleSort(int arr[], int n)
 {
-    int i, j;
-    bool flag;
-    // Outer pass
-    for (i = 0; i < n; i++)
-    {
-        flag = false;
-        for (j = 0; j < n - i - 1; j++)
-        {
-            // Compare values
-            if (arr[j] > arr[j + 1])
-            {
-                swap(arr[j], arr[j + 1]);
-                flag = true;
-            }
-        }
+	int i, j;
+	for (i = 0; i < n - 1; i++)
 
-        if (!flag)
-        {
-            break;
-        }
-    }
+		// Last i elements are already in place
+		for (j = 0; j < n - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+				swap(&arr[j], &arr[j + 1]);
 }
-int main(int argv, char *argc[])
+
+/* Function to print an array */
+void printArray(int arr[], int size)
 {
-    int arr[] = {1, 5, 6, 8, 3, 4, 7, 2, 9};
-    int n = sizeof(arr) / sizeof(int);
-    cout << "Unsorted Array :";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-    bubbleSort(arr, n);
-    cout << "Sorted Array :";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    return (0);
+	int i;
+	for (i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+
+// Driver program to test above functions
+int main()
+{
+	int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	bubbleSort(arr, n);
+	printf("Sorted array: \n");
+	printArray(arr, n);
+	return 0;
 }
