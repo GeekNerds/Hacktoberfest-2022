4c54e6d1ef8c8edf2f643662776e9dd37ae090e5
Soham Chowdhury <chowdhurysoham007@gmail.com>
2022-10-15 20:15:31 +0530

    Update Swap.java

--- DSA/BubbleSort/Swap.java
+++ DSA/BubbleSort/Swap.java
@@ -1,11 +1,24 @@
-public class Swap {
-    public static void swap(int[] arr, int indexOne, int indexTwo) {
-      int temp = arr[indexOne];
-      arr[indexOne] = arr[indexTwo];
-      arr[indexTwo] = temp;
-    }
-  
+public class SwapNumbers {
+
     public static void main(String[] args) {
-  
+
+        float first = 1.20f, second = 2.45f;
+
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+
+        // Value of first is assigned to temporary
+        float temporary = first;
+
+        // Value of second is assigned to first
+        first = second;
+
+        // Value of temporary (which contains the initial value of first) is assigned to second
+        second = temporary;
+
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
     }
-  }
+}
