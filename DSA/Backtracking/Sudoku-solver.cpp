52a00ceb264bfd4b31951b82b6c52410251fd8bd
kriti2020 <96840464+kriti2020@users.noreply.github.com>
2022-10-18 08:07:47 +0530

    backtracking-q2

---
+++ DSA/Backtracking/Sudoku-solver.cpp
@@ -0,0 +1,55 @@
+class Solution
+{
+public:
+    void solveSudoku(vector<vector<char>> &board)
+    {
+        solve(board);
+    }
+
+    bool solve(vector<vector<char>> &board)
+    {
+        for (int i = 0; i < board.size(); i++)
+        {
+            for (int j = 0; j < board[0].size(); j++)
+            {
+                if (board[i][j] == '.')
+                {
+
+                    for (char c = '1'; c <= '9'; c++)
+                    {
+                        if (isPossible(board, i, j, c))
+                        {
+                            board[i][j] = c;
+
+                            if (solve(board) == true)
+                                return true;
+
+                            else
+                                board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool isPossible(vector<vector<char>> &board, int row, int col, char c)
+    {
+
+        for (int i = 0; i < 9; i++)
+        {
+            if (board[row][i] == c)
+                return false;
+
+            if (board[i][col] == c)
+                return false;
+
+            // row i/3 col i%3
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+                return false;
+        }
+        return true;
+    }
+};
