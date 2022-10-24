4de636d473e07a436803b0c00017c086d35eb824
Anmol Rastogi <81026727+Anmol-R@users.noreply.github.com>
2022-10-24 08:59:26 +0530

    Polymorphism use case
    2 classes added to explain polymorphism

--- OOP/PolynomialUse.java
+++ OOP/PolynomialUse.java
@@ -1,1 +1,28 @@
+// Use case of Polymorphism in JAVA
 
+class classPoly {
+ 
+    // Method with 2 integer parameters
+    static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+ 
+    // Method 2
+    // With same name but with 1 double parameters
+    static double Multiply(int a, double b)
+    {
+        return a * b;
+    }
+}
+
+// Main class
+class main {
+ 
+    // Main driver method
+    public static void main(String[] args)
+    {
+        System.out.println(classPoly.Multiply(2, 4));
+        System.out.println(classPoly.Multiply(5.5, 6.3));
+    }
+}
