4d08413863fa788076c81b2df6846cc7d30cbea0
Om <ommukherjee28@gmail.com>
2022-10-27 00:17:37 +0530

    Distinguised between Disks and tower
    Disks and towers were both taken numerically which might cause confusion. Therefore, named disks as characters for better understandability. Also added input function.

--- DSA/TowerofHanoi_recursion.cpp
+++ DSA/TowerofHanoi_recursion.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
-int TOH(int n, int A, int B ,int C){
+int TOH(int n, char from_, char to_ ,char aux_){
     if(n>0){
-        TOH(n-1,A,C,B);
-        printf("(%d %d)\n",A,C);
-        TOH(n-1,B,A,C);
+        TOH(n-1,from_,aux_,to_);
+        printf("Move disk %d from %c to %c\n",n,from_,to_);
+        TOH(n-1,aux_,to_,from_);
     }
 }
 int main(){
-    cout<<TOH(3,1,2,3);
+    int num;
+    cin>>num;
+    cout<<TOH(num,'A','B','C');
     return 0;
 }
-
