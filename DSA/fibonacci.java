f85236696f5fbdb75175aaea3054a4d8cb40aa3b
GeekNerds <114431950+GeekNerds@users.noreply.github.com>
2022-10-28 19:52:27 +0530

    Revert "Spycontribute"

--- DSA/fibonacci.java
+++ DSA/fibonacci.java
@@ -4,16 +4,17 @@ class fibonacci
     public static void main(String[] args)
     {
       Scanner sc = new Scanner(System.in);
-
-      System.out.println("Enter the limit number");
-      int n = sc.nextInt();
-      System.out.println(fib(n));
+      System.out.println("Enter the number");
+      int x = sc.nextInt();
+      int fib_sum = fib(x);
+      System.out.println(fib_sum);
+      sc.close();  
     }
-    public static int fib(int x)
+    public static int fib(int a)
     {
-       if(x<=1)
-       return x;
-       else
-       return fib(x-1)+fib(x-2);
+        if(a<=1)
+        return a;
+        else
+        return fib(a-1) + fib(a-2);
     }
 }
