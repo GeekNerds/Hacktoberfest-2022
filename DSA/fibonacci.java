7c1880b00ab601ea2f02138493bfd3850d2ed953
Himanshu <singhhimanshu1220@gmail.com>
2022-10-28 18:11:43 +0530

    Add fibonacci program in java

---
+++ DSA/fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class fibonacci
+{
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the limit number");
+      int n = sc.nextInt();
+      System.out.println(fib(n));
+    }
+    public static int fib(int x)
+    {
+       if(x<=1)
+       return x;
+       else
+       return fib(x-1)+fib(x-2);
+    }
+}
